bb92457a995d9ceecf09efe382ea4535ede64572
Radhika <rvramayra1998@gmail.com>
2023-07-25 09:45:49 +0530

    asdf

--- server/Connection/Connetion.js
+++ server/Connection/Connetion.js
@@ -4,16 +4,16 @@ const dotenv = require("dotenv");
 // dotenv.config({ path: "../config/config.env" });
 dotenv.config();
 // console.log(process.env.MONGOOSE_URL)
-exports.Connection = ()=>{
-     mongoose.connect(process.env.MONGOOSE_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then((data) => {
-        console.log("connection is successfull");
-      })
-      .catch((err) => {
-        console.log("No connection", err);
-      });
-}
-
+exports.Connection = () => {
+  mongoose
+    .connect(process.env.MONGOOSE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log("connection is successfull");
+    })
+    .catch((err) => {
+      console.log("No connection", err);
+    });
+};
